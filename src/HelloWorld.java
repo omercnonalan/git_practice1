3fda1cf72d99956d453761e183ade9dccaf9bea2
omercanonalan <omercnonalan@gmail.com>
2022-04-22 00:58:42 +0100

    initial commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,8 @@
 public class HelloWorld {
 
+    //doing some work here (you can type anything you want)
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
 
 }
